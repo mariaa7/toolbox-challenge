4c10092aae9024548a2f0a8987662854bf2f9fc3
mariaa7 <mariaa7@uw.edu>
2014-11-24 21:31:26 -0800

    Modified main CSS, index HTML, and jquery JS

--- js/jquery.js
+++ js/jquery.js
@@ -6,11 +6,7 @@ var flippedCard;
 var count = 0;
 var matches = [];
 var failedAttempts = 1;
-
-console.log(array);
-
-
-var flipped = false;
+var startTime = _.now();
 
 var game = $('#game-board');
 setTiles();
@@ -22,43 +18,36 @@ function setTiles() {
 	shuffled = shuffled.slice(0, 8);
 	shuffled = $.merge(shuffled, shuffled);
 	shuffled = _.shuffle(shuffled);
-	console.log(shuffled);
 	for (var i = 0; i < 16; i++) {
 		var newTile = $(document.createElement('img'));
 		newTile.attr('src', 'img/tile-back.png');
 		newTile.attr('width', '200px');
-		newTile.attr('border', '5px solid black');
 		newTile.data('tile-front', shuffled[i].src);
-		
 		newTile.data('matched', false);
 		game.append(newTile);
 	}
 }
 
-//flips card to show image
-//$('#game-board img').click(function() {
+//flips card to show image and checks for a match if one is already flipped
 function clickTiles() {
 	var clickedImg = $(this);
-	if (!flipped && _.contains(matches, clickedImg.data('tile-front')) === false && count == 0
+	if (_.contains(matches, clickedImg.data('tile-front')) === false && count == 0
 		&& clickedImg.hasClass('flipped') === false) {
 		clickedImg.attr('src', clickedImg.data('tile-front'));
-		flipped = true;
 		$(this).addClass('flipped');
 		flippedCard = clickedImg;
 		count++;
 	} else if (!clickedImg.data('matched') && count < 2 && _.contains(matches, clickedImg.data('tile-front')) === false
 		&& clickedImg.hasClass('flipped') === false){
-		flipped = false;
 		count++;
 		clickedImg.attr('src', clickedImg.data('tile-front'));
 		if (clickedImg.data('tile-front') == flippedCard.data('tile-front')){
 			matches.push(clickedImg.data('tile-front'));
-			console.log(matches);
 			count = 0;
 			$('#matches').html("Total Matches: " + matches.length);
 			$('#remainingMatches').html("Remaining Matches: " + (8 - matches.length));
 			if(matches.length === 8) {
-				alert("You Won!"); //change to function
+				alert('You Won!');
 			}
 		} else {
 			setTimeout(function(){flippedCard.attr('src', 'img/tile-back.png')}, 1000);
@@ -68,26 +57,26 @@ function clickTiles() {
 			$('#failedAttempts').html("Failed Attempts: " + failedAttempts++);
 		}
 	}
-//});
 }
 
-//not working, doesn't flip on click
+//Starts a new game
 $('#newGame').click(function() {
 	game.empty();
 	matches = [];
+	failedAttempts = 1;
+	count = 0;
 	$('#matches').html("Total Matches: " + 0);
 	$('#failedAttempts').html("Failed Attempts: " + 0);
 	$('#remainingMatches').html("Remaining Matches: " + (8 - matches.length));
 	setTiles();
 	$('#game-board img').click(clickTiles);
+	startTime = _.now();
+	onTimer();
 });
 
-/*var startTime = _.now();
+//Set timer in seconds
 var timer = window.setInterval(onTimer, 1000);
 function onTimer() {
 	var totalSecs = Math.floor((_.now() - startTime)/ 1000);
-	$('#time').html(totalSecs);
-}*/
-
-//makes all tiles tile back
-//$('#game-board img').attr('src', 'img/tile-back.png');
+	$('#time').html("Seconds: " + totalSecs);
+}
